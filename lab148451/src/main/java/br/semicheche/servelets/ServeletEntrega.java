0a9cfd852ef0a8823ce2048de8948d3c497b1128
Semicheche <lucianosemicheche@gmail.com>
2016-08-11 22:02:55 -0300

    #2 Criado os servelets para venda e entrega e adicionado o metodo doGet.

---
+++ lab148451/src/main/java/br/semicheche/servelets/ServeletEntrega.java
@@ -0,0 +1,25 @@
+package br.semicheche.servelets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/entrega")
+public class ServeletEntrega extends HttpServlet {
+
+	/**
+	 * @author luciano
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+}
